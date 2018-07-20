c3be0140ba68c13f394ac1a14838288d29000fb2
Gavin Goldsmith <gavgold@outlook.com>
2018-07-20 14:40:21 +0200

    Updated to use Click as well as to display more information per instance

--- snapshotanaly/snapshotanaly.py
+++ snapshotanaly/snapshotanaly.py
@@ -1,4 +1,7 @@
+#! /Users/Gavin/.local/share/virtualenvs/snapshot-analyser-qpeeRxI0/bin/python3
+
 import boto3
+import click
 
 # Always create a session connection to AWS
 # Create relevant resources
@@ -6,9 +9,21 @@ session = boto3.Session(profile_name='pytrain')
 ec2 = session.resource('ec2')
 
 # List ec2 instances
+@click.command()
 def list_instamces():
+    "List EC2 instances"
+  
     for i in ec2.instances.all():
-        print(i.instance_id,i.key_name,i.state)
+        print('|'.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            i.private_ip_address,
+            i.tags[0]['Value'])))
+
+    return()
 
 if __name__ == '__main__':
     list_instamces()
