1e0243c3ac69b65c134542f8c14051d90f0b9365
Gavin Goldsmith <gavgold@outlook.com>
2018-07-20 08:46:56 +0200

    update Python from 3.6.1. to 3.7.0

--- snapshotanaly/snapshotanaly.py
+++ snapshotanaly/snapshotanaly.py
@@ -1,9 +1,14 @@
 import boto3
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='pytrain')
-    ec2 = session.resource('ec2')
+# Always create a session connection to AWS
+# Create relevant resources
+session = boto3.Session(profile_name='pytrain')
+ec2 = session.resource('ec2')
 
+# List ec2 instances
+def list_instamces():
     for i in ec2.instances.all():
-        print(i)
+        print(i.instance_id,i.key_name,i.state)
 
+if __name__ == '__main__':
+    list_instamces()
